2b66d8f484d581d0a939d45ab7b8373b2f75c71e
manish77s <69430012+manish77s@users.noreply.github.com>
2023-07-15 23:29:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// function myfunction(){
+//     console.log("hello world")
+// }
+
+
+// function Sum(a,b){
+//     console.log(a+b)
+// }
+
+// Sum(3,7)
+
+// const result = ()=>{
+//     return "hello"
+// }
+
+//Print output: 
+var x = 21;
+var girl = function () {
+    console.log(x);
+    var x = 20;
+};
+girl ();
+
